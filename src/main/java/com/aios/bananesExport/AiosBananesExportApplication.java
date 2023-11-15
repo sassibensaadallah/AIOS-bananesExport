b4397d6bfb4a2fe032b6e6093cba5b24dfbab655
unknown <saadallahsassi@gmail.com>
2023-11-15 08:50:21 +0100

    project init

---
+++ src/main/java/com/aios/bananesExport/AiosBananesExportApplication.java
@@ -0,0 +1,13 @@
+package com.aios.bananesExport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiosBananesExportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiosBananesExportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aios/bananesExport/AiosBananesExportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aios.bananesExport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiosBananesExportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
